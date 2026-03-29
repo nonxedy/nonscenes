e7cc95a8cb831aee0b7f9e68ee4d21a214ea79f5
nonxedy <87914821+nonxedy@users.noreply.github.com>
2026-03-29 17:27:43 +0300

    chore: update Paper API to 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 
 dependencies {
     // Paper API
-    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
 
     // LuckPerms API
     compileOnly("net.luckperms:api:5.4")
